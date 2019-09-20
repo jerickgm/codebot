f0435e046ca99f464662059cbdb26c07726707ce
jerickgm <erick_surface@hotmail.com>
2019-09-20 16:10:41 -0500

    agregando funciones
    ya puede ingresar a un canal de voz con el comando ;ingresar

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var prefix = config.prefix;
 
 client.on("ready", () =>{
     console.log(`${client.user.username} esta preparado!`);
-    client.user.setActivity("programando!!!!.")
+    client.user.setActivity('programando')
 });
 
 client.on("message", async message => {
@@ -14,6 +14,8 @@ client.on("message", async message => {
     const comando = args.shift().toLowerCase();
     
     if (!message.content.startsWith(prefix)) return;
+
+    //Para responder en el canal
     if (message.author.bot) return;
     
     switch (comando) {
@@ -24,15 +26,31 @@ client.on("message", async message => {
             message.reply("leyendo un poco de codigo");
             break;
         case "adios":
-            message.channel.send("Hasta luego!");
+            message.reply("Hasta luego!");
             break;
         case "mama_huevo":
-            message.channel.send("Tu eres un mmgvo!!");
+            message.reply("Tu eres un mmgvo!!");
             break;
         case "puto_volviste!!":
-            message.channel.send("Me fui a depurar humano invesil");
+            message.reply("Me fui a depurar humano invesil");
             break;
     };
+
+    //Para conectarse a un canal de voz
+    if (!message.guild) return;
+
+    if (comando === 'ingresa') {
+    // Only try to join the sender's voice channel if they are in one themselves
+        if (message.member.voiceChannel) {
+        message.member.voiceChannel.join()
+            .then(connection => { // Connection is an instance of VoiceConnection
+            message.reply('¿Para que me necesitas humano?');
+            })
+            .catch(console.log);
+        } else {
+        message.reply('Ingresa primero a un canal de voz, estupido humano');
+        }
+    }
 });
 
 
